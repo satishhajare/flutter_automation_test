06669cb68c6988c72c922dcc9b6f19481e9aa71d
Satyawan_Hajare <satyawan.hajare@capgemini.com>
2022-03-15 12:58:15 +0530

    remove unwanted code

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,11 +1,8 @@
 import 'package:flutter_automation_poc/main.dart';
-//import 'package:flutter_automation_poc/model/technology_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:integration_test/integration_test.dart';
-
-//import 'package:provider/provider.dart';
 
 Future<void> addDelay(int ms) async {
   await Future<void>.delayed(Duration(milliseconds: ms));
@@ -61,6 +58,7 @@ void main() {
       await logout(tester);
     });
 
+    //Login screen test
     testWidgets('Modifying Features test', (WidgetTester tester) async {
       await Firebase.initializeApp();
       await tester.pumpWidget(MyApp());
@@ -78,40 +76,6 @@ void main() {
       await addDelay(2000);
       tester.printToConsole('New Technology screen opens');
       await tester.pumpAndSettle();
-
-      // await tester.enterText(
-      //     find.byKey(const ValueKey('technologyField')), 'Android');
-      // await tester.enterText(
-      //     find.byKey(const ValueKey('descriptionField')), 'Open Source');
-
-      // await addDelay(4000);
-
-      // await tester.ensureVisible(find.byType(ElevatedButton));
-      // await tester.pumpAndSettle();
-      // await tester.tap(find.byType(ElevatedButton));
-      // await addDelay(4000);
-
-      // tester.printToConsole('New Technology added!');
-      // await tester.pumpAndSettle();
-      // await addDelay(1000);
-
-      // final state = tester.state(find.byType(Scaffold));
-      // final title = Provider.of<TechnologyModel>(state.context, listen: false)
-      //     .getAllIdeas[0]
-      //     .title;
-      // await addDelay(1000);
-      // final temp = title!;
-      // await tester.drag(
-      //   find.byKey(ValueKey(
-      //     title.toString(),
-      //   )),
-      //   const Offset(-600, 0),
-      // );
-      // await addDelay(5000);
-
-      // expect(find.text(temp), findsNothing);
-      // tester.printToConsole('Record Deleted Successfully!!');
-      // await logout(tester);
     });
   });
 }
